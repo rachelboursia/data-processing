be98661e017c282ba6a48a88b0077d064cb238ea
Rachel Boursia <91563448+rachelboursia@users.noreply.github.com>
2022-01-24 20:24:49 -0800

    adds cart

--- js/data-processing.js
+++ js/data-processing.js
@@ -1,22 +1,58 @@
 //use location object to access querystring (address bar)
 const queryString = window.location.search;
+let myData = '';//will store data for output
+let myCart = '';//will store cart details
+let myTotal = 0;//will store total cost
     
-//output to console    
-//console.log(queryString);
+if(queryString != ""){//process data
+
+  //separate querystring parameters
+  const urlParams = new URLSearchParams(queryString);
+
+  /*
+    Print all data to id of output to page
+  */
+
+
+  urlParams.forEach(function(value, key) {
+      //https://stackoverflow.com/questions/542232/in-javascript-how-can-i-perform-a-global-replace-on-string-with-a-variable-insi
+      //will replace underscore with spaces
+
+
+      if(key=="Cart"){//Process cart
+         switch(value){
+           case "Widget":
+            myCart += "Widget: 3.99<br />"
+            myTotal += 3.99;
+          break;
+
+          case "Sprocket":
+            myCart += "Sprocket: 5.99<br />"
+            myTotal += 5.99;
+          break;
+
+          case "Thingy":
+            myCart += "Thingy: 1.99<br />"
+            myTotal += 1.99;
+          break;
+         }
+      }else{//Build Shipping Info
+          key = key.split("_").join(" ");
+          myData += `<p>${key}: ${value}<p>`;
+      //console.log(value, key);
+      }
+ 
+  });
+
+  myCart = `
+    <p><b>Cart Contents</b></p>
+    <p>${myCart}</p>
+    <p>Total: $${myTotal}</p>
+  `;
+  myData = myCart + "<p><b>Shipping Information</b></p>" + myData;
+  myData += `<p><a href="index.html">CLEAR</a></p>`
+ 
+  document.getElementById("output").innerHTML = myData; 
     
-//separate querystring parameters
-const urlParams = new URLSearchParams(queryString);
-
-let myData = '';
-urlParams.forEach(function(value, key) {
+}
     
-    //https://stackoverflow.com/questions/542232/in-javascript-how-can-i-perform-a-global-replace-on-string-with-a-variable-insi
-    //will replace underscore with spaces
-   key = key.split("_").join(" ");
-
-    myData += `<p>${key}: ${value}<p>`;
-   //console.log(value, key);
- });
-
-myData += `<p><a href="index.html">CLEAR</a></p>`
- document.getElementById("output").innerHTML = myData; 
